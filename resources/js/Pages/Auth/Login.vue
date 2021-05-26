9fdaf633ff199984f7fbf63639c538a757bf3d9c
Ruben Steen <RubenSteen@users.noreply.github.com>
2021-05-26 17:40:46 +0000

    Apply fixes from StyleCI

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -24,7 +24,25 @@
                                 'border-red-300 placeholder-red-500 text-red-900 focus:border-red-300':
                                     $page.errors.username,
                             }"
-                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                            class="
+                                appearance-none
+                                rounded-none
+                                relative
+                                block
+                                w-full
+                                px-3
+                                py-2
+                                border border-gray-300
+                                placeholder-gray-500
+                                text-gray-900
+                                rounded-t-md
+                                focus:outline-none
+                                focus:shadow-outline-blue
+                                focus:border-blue-300
+                                focus:z-10
+                                sm:text-sm
+                                sm:leading-5
+                            "
                             placeholder="Username"
                         />
                     </div>
@@ -38,7 +56,25 @@
                                 'border-red-300 placeholder-red-500 text-red-900 focus:border-red-300':
                                     $page.errors.password,
                             }"
-                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                            class="
+                                appearance-none
+                                rounded-none
+                                relative
+                                block
+                                w-full
+                                px-3
+                                py-2
+                                border border-gray-300
+                                placeholder-gray-500
+                                text-gray-900
+                                rounded-b-md
+                                focus:outline-none
+                                focus:shadow-outline-blue
+                                focus:border-blue-300
+                                focus:z-10
+                                sm:text-sm
+                                sm:leading-5
+                            "
                             placeholder="Password"
                         />
                     </div>
@@ -62,11 +98,42 @@
                 <div class="mt-6">
                     <button
                         type="submit"
-                        class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                        class="
+                            group
+                            relative
+                            w-full
+                            flex
+                            justify-center
+                            py-2
+                            px-4
+                            border border-transparent
+                            text-sm
+                            leading-5
+                            font-medium
+                            rounded-md
+                            text-white
+                            bg-indigo-600
+                            hover:bg-indigo-500
+                            focus:outline-none
+                            focus:border-indigo-700
+                            focus:shadow-outline-indigo
+                            active:bg-indigo-700
+                            transition
+                            duration-150
+                            ease-in-out
+                        "
                     >
                         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                             <svg
-                                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                                class="
+                                    h-5
+                                    w-5
+                                    text-indigo-500
+                                    group-hover:text-indigo-400
+                                    transition
+                                    ease-in-out
+                                    duration-150
+                                "
                                 fill="currentColor"
                                 viewBox="0 0 20 20"
                             >
